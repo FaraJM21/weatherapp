a67baf57ac4864b13761d71f4bb6075be1b1a9da
FaraJM21 <farruxrihsiyev@gmail.com>
2022-10-31 20:43:10 +0500

    api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
   TopButtons,
@@ -7,7 +7,18 @@ import {
   Inputs,
   Forecast,
 } from "./components/index";
+import { getFormattedWeatherData } from "./services/weatherService";
+
 function App() {
+  const fetchWeatherData = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
