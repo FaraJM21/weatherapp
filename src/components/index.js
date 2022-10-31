4d52220337039c06813146b82e766c7cc1575c28
FaraJM21 <farruxrihsiyev@gmail.com>
2022-10-31 18:25:01 +0500

    UX/UI temp;ate finished

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as TopButtons } from "./TopButtons";
+export { default as TimeAndLocation } from "./TimeAndLocation";
+export { default as TemperatureAndDetails } from "./TemperatureAndDetails";
+export { default as Inputs } from "./Inputs";
+export { default as Forecast } from "./Forecast";
